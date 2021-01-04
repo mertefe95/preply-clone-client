43a48ff3e71e7b147abf7665bd8e8656479ca07d
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2021-01-04 15:55:35 +0300

    new cmmt

---
+++ src/theme.js
@@ -0,0 +1,11 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+
+export const theme = createMuiTheme({
+  palette: {
+    secondary: {
+      main: "#35a1aa",
+      light: "#63c3cb"
+    }
+  }
+});
